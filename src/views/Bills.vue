cbf31cce1ff2326f2760c4d7bf78e66a9e3d986e
hannah-a <hannahlinarmstrong@gmail.com>
2025-02-22 01:05:13 -0700

    update bills

--- src/views/Bills.vue
+++ src/views/Bills.vue
@@ -2,7 +2,7 @@
     <div class="bills py-12 px-4 max-w-screen-2xl">
         <h1 class="py-4">2025 General Session Bills</h1>
         <div class="flex flex-col">
-            <span>Last updated: 2/20/25 1:36 A.M. MDT</span>
+            <span>Last updated: 2/22/25 1:00 A.M. MDT</span>
             <span>Small bug where the resolution bills (HR/SR) don't show the correct status, will fix soon!</span>
         </div>
 
